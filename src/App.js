90d790de89f0cbbcaea525ca55ca319b936bd612
Kashish <kashishverma8382@gmail.com>
2023-07-01 12:41:12 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {createBrowserRouter,RouterProvider} from 'react-router-dom';
 import HomePage from './pages/Home';
 import Products from './pages/Products';
+import RootLayout from './pages/Root';
 const router=createBrowserRouter([
   {
     path:'/',element:<RootLayout></RootLayout>
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,1 +1,13 @@
-function RootLayout()
+import {Outlet} from 'react-router-dom';
+import classes from './Root.module.css';
+import MainNavigation from '../components/MainNavigation';
+function RootLayout(){
+    return <>
+    <MainNavigation></MainNavigation>
+    <main className= {classes.content}>
+    <Outlet></Outlet>
+    </main>
+    </>
+    
+}
+export default RootLayout;
