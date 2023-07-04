5469fd4e3f2a5754dc55fae3c346fea1242581a1
Kashish <kashishverma8382@gmail.com>
2023-07-04 06:51:23 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import {createBrowserRouter,RouterProvider} from 'react-router-dom';
 import HomePage from './pages/Home';
 import Products from './pages/Products';
 import RootLayout from './pages/Root';
+import ErrorPage from './pages/ErrorPage';
 const router=createBrowserRouter([
   {
-    path:'/',element:<RootLayout></RootLayout>
+    path:'/',element:<RootLayout></RootLayout>,
+    errorElement:<ErrorPage></ErrorPage>
     ,children:[ {path:'/',element:<HomePage></HomePage>},
     {path:'/products',element:<Products></Products>}]
   }
---
+++ src/pages/Error.js
@@ -0,0 +1,7 @@
+import MainNavigation from "../components/MainNavigation";
+function ErrorPage(){
+    return <>
+    <main><h1>an error occoured</h1></main></>
+}
+
+export default ErrorPage;
